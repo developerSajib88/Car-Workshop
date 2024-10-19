7479f0895d42ca51886228cef0bf68dcc1fd1f36
developerSajib88 <developersajib88@gmail.com>
2024-10-19 13:19:54 +0600

    get local database create.

--- lib/data/local_database/get_local_database.dart
+++ lib/data/local_database/get_local_database.dart
@@ -1,1 +1,23 @@
-class GetLocalDatabase {}
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'src_local_database.dart';
+
+class GetLocalDatabase {
+
+  final database = Hive.box(KeyLocalDatabase.databaseName);
+
+  bool? userIsLogIn() => database.get(KeyLocalDatabase.userIsLogIn);
+
+  bool? userRegister() => database.get(KeyLocalDatabase.userRegister);
+
+  String? userName() => database.get(KeyLocalDatabase.userName);
+
+  String? userEmail() => database.get(KeyLocalDatabase.userEmail);
+
+  String? userPhone() => database.get(KeyLocalDatabase.userPhone);
+
+  String? userAccessToken() => database.get(KeyLocalDatabase.userAccessToken);
+
+  String? userRefreshToken() => database.get(KeyLocalDatabase.userRefreshToken);
+
+}
