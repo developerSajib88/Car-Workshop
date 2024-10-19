ec4135f4f5a6921c4f7b203e4946f4be0d01bc1e
developerSajib88 <developersajib88@gmail.com>
2024-10-19 13:32:15 +0600

    splash_screen.dart create.

---
+++ lib/features/splash/splash_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  static String get path => "/splashScreen";
+  static String get name => "splashScreen";
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
