ef039f6df22ada6d54564614ec64f972e5edf3b2
developerSajib88 <developersajib88@gmail.com>
2024-10-19 13:19:37 +0600

    dependency injection implement.

---
+++ lib/core/dependency_injection/dependency_injection.dart
@@ -0,0 +1,9 @@
+
+import 'package:get_it/get_it.dart';
+part 'dependency_injection_provider.dart';
+
+final getIt = GetIt.instance;
+
+void init() {
+
+}
---
+++ lib/core/dependency_injection/dependency_injection_provider.dart
@@ -0,0 +1,6 @@
+part of 'dependency_injection.dart';
+
+/// This is my riverpod state provider
+// final videoPlayerProvider =
+// StateNotifierProvider.autoDispose<VideoPlayerNotifier,VideoPlayerState>((ref) =>
+// VideoPlayerNotifier(videoPlayerDomain: singleTon())..init());
