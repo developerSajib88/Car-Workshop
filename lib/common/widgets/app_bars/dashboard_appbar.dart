612f7f06c5d7e6ac3c64f0de4832515dfdef0843
developerSajib88 <developersajib88@gmail.com>
2024-10-19 19:03:13 +0600

    Dashboard app bar create.

---
+++ lib/common/widgets/app_bars/dashboard_appbar.dart
@@ -0,0 +1,70 @@
+
+import 'package:feature_first/common/widgets/buttons/primary_buttons.dart';
+import 'package:feature_first/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class DashboardAppBar extends HookConsumerWidget implements PreferredSizeWidget {
+  final bool? showActionButton;
+  final bool? isNotificationButton;
+  const DashboardAppBar(
+      {super.key, this.showActionButton, this.isNotificationButton});
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size(1.sw, 40.h);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      width: 1.sw,
+      color: ColorPalates.primaryColor,
+      padding: padding4,
+      child: SafeArea(
+        child: Row(
+          children: [
+            CircleAvatar(
+              child: Icon(Icons.account_circle_rounded),
+            ),
+        
+            gap8,
+        
+            Column(
+              crossAxisAlignment: crossStart,
+              mainAxisAlignment: mainCenter,
+              children: [
+                Text(
+                  "Sajib Hasan",
+                  style: CustomTextStyles.primaryTextStylesBold.copyWith(
+                      color: ColorPalates.defaultWhite
+                  ),
+                ),
+        
+                Text(
+                  "Admin",
+                  style: CustomTextStyles.descriptionTextStyles.copyWith(
+                      color: Colors.white
+                  ),
+                )
+        
+              ],
+            ),
+
+            const Spacer(),
+        
+            SizedBox(
+              width: 60.w,
+              height: 22.h,
+              child: PrimaryButton(
+                  title: "Book Now",
+                  backgroundColor: ColorPalates.defaultWhite,
+                  titleColor: ColorPalates.secondaryColor,
+                  onPressed: (){}
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
