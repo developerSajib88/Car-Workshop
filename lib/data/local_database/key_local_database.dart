14a8044221812a17109177f6ede735e7df9f02ad
developerSajib88 <developersajib88@gmail.com>
2024-10-19 13:20:15 +0600

    key local database create.

--- lib/data/local_database/key_local_database.dart
+++ lib/data/local_database/key_local_database.dart
@@ -1,1 +1,19 @@
-class KeyLocalDatabase {}
+class KeyLocalDatabase {
+
+  static String get databaseName => "AdMobility";
+
+  static String get userIsLogIn => "userIsLogin";
+
+  static String get userRegister => "userRegister";
+
+  static String get userName => "userName";
+
+  static String get userEmail => "userEmail";
+
+  static String get userPhone => "userPhone";
+
+  static String get userAccessToken => "userAccessToken";
+
+  static String get userRefreshToken => "userRefreshToken";
+
+}
