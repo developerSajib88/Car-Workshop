af10e55a3be43ce3b694b43ed84b3d51b593a4dc
developerSajib88 <developersajib88@gmail.com>
2024-10-20 20:41:13 +0600

    Booking list view create.

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -1,5 +1,9 @@
 import 'package:feature_first/common/widgets/app_bars/dashboard_appbar.dart';
+import 'package:feature_first/common/widgets/buttons/widget_bounce.dart';
+import 'package:feature_first/common/widgets/components/booking_item_view.dart';
+import 'package:feature_first/utils/constants/ui_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -7,8 +11,21 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DashboardAppBar(),
-      body: Column(),
+      appBar: const DashboardAppBar(),
+      body: Container(
+        width: 1.sw,
+        height: 1.sh,
+        padding: padding6,
+        child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context,index){
+              return WidgetBounce(
+                  onPressed: (){},
+                  child: const BookingItemView()
+              );
+            }
+        ),
+      ),
     );
   }
 }
