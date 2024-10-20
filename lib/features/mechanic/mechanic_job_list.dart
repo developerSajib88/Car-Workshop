1b40aa77a70ed31638c1c9e5890b4e8ec74061a8
developerSajib88 <developersajib88@gmail.com>
2024-10-20 23:47:37 +0600

    Mechanic job list create.

--- lib/features/mechanic/mechanic_job_list.dart
+++ lib/features/mechanic/mechanic_job_list.dart
@@ -1,4 +1,7 @@
+import 'package:feature_first/common/widgets/buttons/widget_bounce.dart';
 import 'package:feature_first/common/widgets/components/item_view/mechanic_job_item_view.dart';
+import 'package:feature_first/features/common/booking_job_details/booking_job_details_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -9,7 +12,13 @@ class MechanicJobList extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return ListView.builder(
         itemCount: 10,
-        itemBuilder: (context,index)=> const MechanicJobItemView()
+        itemBuilder: (context,index){
+          return WidgetBounce(
+              onPressed: ()=> Navigator.push(context,
+                  CupertinoPageRoute(builder: (context)=> const BookingJobDetailsScreen())),
+              child: const MechanicJobItemView()
+          );
+        }
     );
   }
 }
