256571d283ae291c4fe0f5fd137ea5f02080c1ee
developerSajib88 <developersajib88@gmail.com>
2024-10-21 20:49:56 +0600

    Firebase configuration for android.

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   di.init();
   await Hive.initFlutter();
   await Hive.openBox(KeyLocalDatabase.databaseName);
