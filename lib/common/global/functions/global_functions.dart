7abc52b168bf7da3b8fcbd7f02ffb990bcccae6d
developerSajib88 <developersajib88@gmail.com>
2024-10-21 23:55:38 +0600

    Global function create for random user id generate.

--- lib/common/global/functions/global_functions.dart
+++ lib/common/global/functions/global_functions.dart
@@ -1,1 +1,10 @@
-class GlobalFunctions {}
+import 'dart:math';
+
+class GlobalFunctions {
+
+  static int generateRandomUserId() {
+    int randomId = 100000 + Random().nextInt(900000);
+    return randomId;
+  }
+
+}
