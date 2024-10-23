c68660dd3ad45eb4b34afb9e0645c2877f7888a4
developerSajib88 <developersajib88@gmail.com>
2024-10-23 21:18:26 +0600

    admin domain infrastructure create.

---
+++ lib/features/admin/infrastructure/admin_domain_i.dart
@@ -0,0 +1,11 @@
+import 'package:feature_first/data/firebase/firebase_services/firebase_services.dart';
+import 'package:feature_first/data/model/user_model.dart';
+import 'package:feature_first/features/admin/domain/admin_domain.dart';
+
+class AdminDomainI extends AdminDomain{
+
+  @override
+  Future<List<UserModel>?> getAllMechanic() async =>
+      await FirebaseServices().getAllMechanic();
+
+}
