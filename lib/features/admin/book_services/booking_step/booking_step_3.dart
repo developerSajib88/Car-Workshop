da3c80f8c39368f7cceef9f01eb8f3c060484006
developerSajib88 <developersajib88@gmail.com>
2024-10-24 01:28:33 +0600

    Booking step 3 validate.

--- lib/features/admin/book_services/booking_step/booking_step_3.dart
+++ lib/features/admin/book_services/booking_step/booking_step_3.dart
@@ -1,3 +1,4 @@
+import 'package:feature_first/common/global/validation/forms_validation.dart';
 import 'package:feature_first/common/widgets/buttons/widget_bounce.dart';
 import 'package:feature_first/common/widgets/text_form_fields/primary_text_form_fields.dart';
 import 'package:feature_first/core/dependency_injection/dependency_injection.dart';
@@ -12,55 +13,81 @@ class BookingStep3 extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
+    GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
     final adminState = ref.watch(adminProvider);
     final adminCtrl = ref.read(adminProvider.notifier);
 
-    return Column(
-      children: [
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: crossStart,
+        children: [
 
-        PrimaryTextFormFields(
-          title: "Name",
-          hintText: "e.g., John Doe",
-          minLines: 4,
-        ),
-
-        gap8,
-
-        PrimaryTextFormFields(
-            title: "Phone",
-            hintText: "e.g., (123) 456-7890"
-        ),
-
-        gap8,
-
-        PrimaryTextFormFields(
-            title: "Email",
-            hintText: "e.g., johndoe@example.com"
-        ),
-
-        gap12,
-
-        WidgetBounce(
-          onPressed: (){
-
-          },
-          child: Container(
-            width: 60.w,
-            height: 25.h,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: radius4
-            ),
-            child: const Text(
-              "Continue",
-              style: TextStyle(color: Colors.white),
-            ),
+          PrimaryTextFormFields(
+            controller: adminState.customerNameController,
+            isRequired: true,
+            title: "Name",
+            hintText: "e.g,John Doe",
+            minLines: 4,
+            validator: (value)=> FormValidation(
+                validationType: ValidationType.name,
+                formValue: value
+            ).validate(),
           ),
-        )
+
+          gap8,
+
+          PrimaryTextFormFields(
+              controller: adminState.customerPhoneController,
+              isRequired: true,
+              title: "Phone",
+              hintText: "e.g,01938327825",
+              validator: (value)=> FormValidation(
+                  validationType: ValidationType.phone,
+                  formValue: value
+              ).validate(),
+          ),
+
+          gap8,
+
+          PrimaryTextFormFields(
+              controller: adminState.customerEmailController,
+              isRequired: true,
+              title: "Email",
+              hintText: "e.g,johndoe@example.com",
+              validator: (value)=> FormValidation(
+                  validationType: ValidationType.email,
+                  formValue: value
+              ).validate(),
+          ),
+
+          gap12,
+
+          WidgetBounce(
+            onPressed: (){
+              if(formKey.currentState!.validate()){
+
+              }
+            },
+            child: Container(
+              width: 60.w,
+              height: 25.h,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: radius4
+              ),
+              child: const Text(
+                "Continue",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
 
 
-      ],
+        ],
+      ),
     );
   }
 }
