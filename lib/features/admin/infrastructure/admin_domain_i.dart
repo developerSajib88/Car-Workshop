6174db1d3b79c3beca8acdf25cdeeb850be9fd41
developerSajib88 <developersajib88@gmail.com>
2024-10-24 22:11:35 +0600

    admin domain infrastructure create for getting admin booking list.

--- lib/features/admin/infrastructure/admin_domain_i.dart
+++ lib/features/admin/infrastructure/admin_domain_i.dart
@@ -1,4 +1,5 @@
 import 'package:feature_first/data/firebase/firebase_services/firebase_services.dart';
+import 'package:feature_first/data/model/booking_model.dart';
 import 'package:feature_first/data/model/user_model.dart';
 import 'package:feature_first/features/admin/domain/admin_domain.dart';
 
@@ -11,5 +12,9 @@ class AdminDomainI extends AdminDomain{
   @override
   Future<bool?> createBookingService({required Map<String, dynamic> body}) async =>
       await FirebaseServices().createBookingServices(body: body);
+
+  @override
+  Future<List<BookingModel>?> getAdminBookingList({required int adminId}) async =>
+      await FirebaseServices().fetchAdminBookings(adminId: adminId);
 
 }
