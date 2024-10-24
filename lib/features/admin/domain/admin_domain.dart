f3064a6f989bf38b1683220fb9586066e2a2946e
developerSajib88 <developersajib88@gmail.com>
2024-10-24 22:11:20 +0600

    admin domain create for getting admin booking list.

--- lib/features/admin/domain/admin_domain.dart
+++ lib/features/admin/domain/admin_domain.dart
@@ -1,6 +1,8 @@
+import 'package:feature_first/data/model/booking_model.dart';
 import 'package:feature_first/data/model/user_model.dart';
 
 abstract class AdminDomain {
   Future<List<UserModel>?> getAllMechanic();
   Future<bool?> createBookingService({required Map<String,dynamic> body});
+  Future<List<BookingModel>?> getAdminBookingList({required int adminId});
 }
